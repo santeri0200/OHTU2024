17769017602ea4435645c0b08d69605e3f444466
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2024-11-10 23:39:32 +0200

    Completed: Week 2, Task 12

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,18 +1,46 @@
 import requests
 from player import Player
 
+class PlayerReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_players(self):
+        response = requests.get(self._url).json()
+        players = []
+
+        for player_dict in response:
+            player = Player(player_dict)
+            players.append(player)
+
+        return players
+
+class PlayerStats:
+    def __init__(self, reader):
+        self._players = reader.get_players()
+
+    def top_scorers_by_nationality(self, nationality):
+        def sort_by_points(player):
+            return player.goals + player.assists
+
+        players = [player for player in self._players if player.nationality == nationality]
+        sorted_players = sorted(
+            players,
+            reverse=True,
+            key=sort_by_points
+        )
+        
+        return sorted_players
+
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
-    response = requests.get(url).json()
-
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        players.append(player)
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
     print(f"Players from FIN\n")
-    [print(player) for player in players if player.nationality == "FIN"]
+    for player in players:
+        print(player)
 
 if __name__ == "__main__":
     main()
