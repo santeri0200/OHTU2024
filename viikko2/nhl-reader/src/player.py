137035824e3534c3c044e26216a82568a8a77c91
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2024-11-10 23:29:57 +0200

    Completed: Week 2, Task 11

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -9,4 +9,4 @@ class Player:
         self.id          = dict['id']
     
     def __str__(self):
-        return f"{self.name} team {self.team} goals {self.goals} assists {self.assists}"
+        return f"{self.name:20} {self.team:4} {self.goals:2} + {self.assists:2} = {self.goals + self.assists}"
