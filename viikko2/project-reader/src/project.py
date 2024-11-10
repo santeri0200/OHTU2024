2e196ad7f6410613dacd62106ae98483515d07e5
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2024-11-10 21:49:36 +0200

    Completed: Week 2, Task 2

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -1,17 +1,19 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(self, name, description, authors, dependencies, dev_dependencies):
         self.name = name
+        self.authors = authors
         self.description = description
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
 
     def _stringify_dependencies(self, dependencies):
-        return ", ".join(dependencies) if len(dependencies) > 0 else "-"
+        return "".join([f"\n- {dep}" for dep in dependencies])
 
     def __str__(self):
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
+            f"\nAuthors: {self._stringify_dependencies(self.authors)}"
             f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
             f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
         )
--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,5 +1,6 @@
 from urllib import request
 from project import Project
+import toml
 
 
 class ProjectReader:
@@ -9,7 +10,15 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        parsed_content = toml.loads(content)
+        poetry_content = parsed_content["tool"]["poetry"]
+
+        name     = poetry_content["name"]
+        desc     = poetry_content["description"]
+        license  = poetry_content["license"]
+        authors  = poetry_content["authors"]
+        deps     = poetry_content["dependencies"]
+        dev_deps = poetry_content["group"]["dev"]["dependencies"]
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(name, desc, authors, deps, dev_deps)
