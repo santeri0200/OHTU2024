1f45260477373f8104f740149d600b6b4b7a7fc9
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2024-11-10 23:26:54 +0200

    Completed: Week 2, Task 10

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,18 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    response = requests.get(url).json()
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+        players.append(player)
+
+    print(f"Players from FIN\n")
+    [print(player) for player in players if player.nationality == "FIN"]
 
 if __name__ == "__main__":
     main()
