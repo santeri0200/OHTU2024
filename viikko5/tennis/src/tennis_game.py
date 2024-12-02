2687a74940962d7f08444e6bed02d8a36c11369c
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2024-12-02 16:40:20 +0200

    Completed: Week 5, Task 3

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -6,50 +6,48 @@ class TennisGame:
         self.m_score2 = 0
 
     def won_point(self, player_name):
-        if player_name == "player1":
+        if player_name == self.player1_name:
             self.m_score1 = self.m_score1 + 1
-        else:
+        elif player_name == self.player2_name:
             self.m_score2 = self.m_score2 + 1
 
     def get_score(self):
         score = ""
-        temp_score = 0
 
         if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            else:
-                score = "Deuce"
+            match self.m_score1:
+                case 0:
+                    score = "Love-All"
+                case 1:
+                    score = "Fifteen-All"
+                case 2:
+                    score = "Thirty-All"
+                case _:
+                    score = "Deuce"
         elif self.m_score1 >= 4 or self.m_score2 >= 4:
             minus_result = self.m_score1 - self. m_score2
-
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
+            match minus_result:
+                case 1:
+                    score = f"Advantage {self.player1_name}"
+                case -1:
+                    score = f"Advantage {self.player2_name}"
+                case minus_result if minus_result >= 2:
+                    score = f"Win for {self.player1_name}"
+                case _:
+                    score = f"Win for {self.player2_name}"
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
+            temp_score = 0
+            def get_name(score):
+                match score:
+                    case 0:
+                        return "Love"
+                    case 1:
+                        return "Fifteen"
+                    case 2:
+                        return "Thirty"
+                    case 3:
+                        return "Forty"
 
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
+            score = f"{get_name(self.m_score1)}-{get_name(self.m_score2)}"
 
         return score
